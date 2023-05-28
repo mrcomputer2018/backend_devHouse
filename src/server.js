880263c0f96ed409f7b9a84d06b9b6fb931cd587
Marcelo Ribeiro <marcelo.ribeiro@proeja.com>
2023-05-28 18:18:31 -0300

    acertos

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const port = process.env.PORT;
+const port = process.env.PORT || 3333;
 
 app.listen(port, () => {
     console.log(`Escutando a porta ${port}`);
