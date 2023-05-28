481356bb2503628969ffd776cfe568785695d26e
Marcelo Ribeiro <marcelo.ribeiro@proeja.com>
2023-05-28 19:09:22 -0300

    conectando ao BD

--- src/app.js
+++ src/app.js
@@ -1,9 +1,29 @@
 import express from 'express';
 import routes from './routes';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const server = express();
 
 server.use(express.json());
+
+async function db(){
+    try {
+
+        await mongoose.connect(process.env.MONGODB_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        console.log(">>> Conectado ao Banco de Dados");
+
+    } catch (error) {
+        console.log(`Erro: ${error.message}`);
+    }
+}
+
+db();
 
 server.use(routes);
 
