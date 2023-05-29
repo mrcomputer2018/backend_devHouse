ee12052e31fcf9d0b2da628c09e4c62fd5d58beb
Marcelo Ribeiro <marcelo.ribeiro@proeja.com>
2023-05-29 18:22:17 -0300

    criando uma sessao

--- src/controllers/SessionController.js
+++ src/controllers/SessionController.js
@@ -7,9 +7,22 @@
     update - atualizar uma sessão
     destroy - quando queremos deletar uma sessão 
 */
+
+import User from '../models/User';
+
 class SessionController{
-    store(req,res) { 
-        return res.json({ message: 'Minha Api' });
+   
+    async store(req,res) { 
+        const email = req.body.email;
+        
+        // Verificando se este usuario ja existe
+        let user = await User.findOne({ email: email });
+
+        if(!user){
+            user = await User.create({ email: email });
+        }
+
+        return res.json(user);
     }
 }
 
