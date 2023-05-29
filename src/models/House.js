bbef387530621567e19565127186608f1c8a9f6e
Marcelo Ribeiro <marcelo.ribeiro@proeja.com>
2023-05-29 18:31:16 -0300

    Criando a model House

---
+++ src/models/House.js
@@ -0,0 +1,16 @@
+import {Schema, model} from 'mongoose';
+
+const HouseSchema = new Schema({
+    thumbmail: String,
+    description : String,
+    price: Number,
+    location: String,
+    status: Boolean,
+    user: {
+        // referenciando ao ID do usuario
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+    }
+});
+
+export default model('House', HouseSchema);
