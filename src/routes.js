e1c37dd75a2a12739923d5e2578236de5a3e2e85
Marcelo Ribeiro <marcelo.ribeiro@proeja.com>
2023-06-01 18:45:10 -0300

    criando rota de upload

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,18 @@
 import { Router } from 'express';
+import multer from 'multer';
+import uploadConfig from './config/upload';
+
 import SessionController from './controllers/SessionController';
 import HouseController from './controllers/HouseController';
 
 const routes = Router();
 
+// iniciando multer
+const multer = multer(uploadConfig);
+
 routes.post('/sessions', SessionController.store);
 
 // Houses
-routes.post('/houses', HouseController.store);
+routes.post('/houses', upload.single('thumbmail'), HouseController.store);
 
 export default routes;
