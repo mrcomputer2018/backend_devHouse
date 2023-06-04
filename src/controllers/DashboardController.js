05fa549db4bf9ffc25cfb4370b9ebb0fda27fef6
Marcelo Ribeiro <marcelo.ribeiro@proeja.com>
2023-06-04 17:55:40 -0300

    criando listagem de todas as casas

---
+++ src/controllers/DashboardController.js
@@ -0,0 +1,14 @@
+import House from "../models/House";
+import User from "../models/User";
+
+class DasnboardController{
+    async show(req,res){
+        const user_id = req.headers.user_id;
+
+        const houses = await House.find({ user: user_id });
+
+        res.json(houses);
+    }
+}
+
+export default new DasnboardController();
--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import uploadConfig from './config/upload';
 
 import SessionController from './controllers/SessionController';
 import HouseController from './controllers/HouseController';
+import DashboardController from './controllers/DashboardController';
 
 const routes = Router();
 
@@ -17,5 +18,8 @@ routes.post('/houses', upload.single('thumbnail'), HouseController.store);
 routes.get('/houses', HouseController.index);
 routes.put('/houses/:house_id', upload.single('thumbnail'), HouseController.update);
 routes.delete('/houses', HouseController.destroy);
+
+//Dashboard
+routes.get('/dashboard', DashboardController.show);
 
 export default routes;
